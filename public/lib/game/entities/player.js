c950670a778d8c21930f9250fc1623a211712caa
Stuart Nelson <stuartnelson3@gmail.com>
2013-03-10 05:12:24 -0500

    animation changes

--- public/lib/game/entities/player.js
+++ public/lib/game/entities/player.js
@@ -10,30 +10,34 @@ EntityPlayer = ig.Entity.extend({
 	size: {x: 8, y:14},
 	offset: {x: 4, y: 2},
 
-	maxVel: {x: 200, y:0 },
+	maxVel: {x: 100, y:0 },
 	friction: {x: 600, y: 0},
 
 	type: ig.Entity.TYPE.A,
 	checkAgainst: ig.Entity.TYPE.NONE,
 	collides: ig.Entity.COLLIDES.PASSIVE,
 
-	animSheet: new ig.AnimationSheet( 'media/player.png', 16, 24 ),
+	animSheet1: new ig.AnimationSheet( 'media/character_with_legs.png', 16, 24 ),
+	animSheet2: new ig.AnimationSheet( 'media/character_attacking.png', 16, 24 ),
 
 	flip: false,
-	accelHoriz: 400,
+	accelHoriz: 500,
 	health: 10,
 
 	init: function( x, y, settings ) {
 		this.parent( x, y, settings )
 
 		// Add the animations
-		this.addAnim( 'idle', 1, [0] )
-		this.addAnim( 'fly', 0.07, [1,2] )
+		this.setAnimationSheet();
 	},
 
+	setAnimationSheet: function() {
+		this.anims.idle = new ig.Animation( this.animSheet1, 1, [0] );
+		this.anims.fly = new ig.Animation( this.animSheet1, 0.07, [1,2]);
+		this.anims.attack = new ig.Animation( this.animSheet2, 5, [0,1,2], true);
+	},
 
 	update: function() {
-
 		this.pos.y = ig.game.screen.y + ((ig.system.height/ 3) * 2)
 
 		// move left or right
@@ -50,24 +54,11 @@ EntityPlayer = ig.Entity.extend({
 
 		// shoot
 		if( ig.input.pressed('shoot') ) {
-			var x = this.pos.x + (this.flip ? -6 : 6 );
+			var x = this.pos.x + (this.flip ? -18 : 6 );
 			var y = this.pos.y + 6;
-			ig.game.spawnEntity( EntityProjectile, x, y, {flip:this.flip} );
+			this.currentAnim = this.anims.attack;
+			ig.game.spawnEntity( EntitySword, x+1, y-14, {flip:this.flip, owner: this} );
 		}
-
-		// set the current animation, based on the player's speed
-		// if( this.vel.y < 0 ) {
-		// 	this.currentAnim = this.anims.jump;
-		// }
-		// else if( this.vel.y > 0 ) {
-		// 	this.currentAnim = this.anims.fall;
-		// }
-		// else if( this.vel.x != 0 ) {
-		// 	this.currentAnim = this.anims.run;
-		// }
-		// else {
-		// 	this.currentAnim = this.anims.idle;
-		// }
 
 		this.currentAnim.flip.x = this.flip;
 
@@ -76,31 +67,39 @@ EntityPlayer = ig.Entity.extend({
 	}
 });
 
-
-EntityProjectile = ig.Entity.extend({
-	size: {x: 8, y: 4},
-	maxVel: {x:200, y:0},
+EntitySword = ig.Entity.extend({
+	size: {x: 24, y: 32},
+  owner: null,
 
 	type: ig.Entity.TYPE.NONE,
 	checkAgainst: ig.Entity.TYPE.B,
-	collides: ig.Entity.COLLIDES.PASSIVE,
+	collides: ig.Entity.COLLIDES.NONE,
 
-	animSheet: new ig.AnimationSheet( 'media/projectile.png', 8, 4 ),
+	animSheet: new ig.AnimationSheet( 'media/swing_full.png', 24, 32 ),
 
 	init: function( x, y, settings ) {
 		this.parent( x, y, settings );
 
-		this.addAnim( 'idle', 1, [0] );
+		this.addAnim( 'swing', 0.07, [0,1,2], true );
 		this.currentAnim.flip.x = settings.flip;
 
-		this.vel.x = (settings.flip ? -this.maxVel.x : this.maxVel.x)
-		this.vel.y = 0
+    this.animationRun = 0;
+	},
+
+	update: function() {
+    var x_offset = this.owner.flip ? -23 : 7.5;
+    this.pos.x = this.owner.pos.x + x_offset;
+    this.pos.y = this.owner.pos.y - 3.5;
+
+		if (this.currentAnim.loopCount && this.animationRun) return this.kill();
+    this.animationRun = 1;
+		this.parent();
 	},
 
 	handleMovementTrace: function( res ) {
 		this.parent( res );
 		if( res.collision.x || res.collision.y ) {
-			this.kill();
+			// this.kill();
 		}
 	},
 
